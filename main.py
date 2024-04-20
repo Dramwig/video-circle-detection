ea15896389218f5e0f84c1f09d8f13232dbdf09f
Wang Zixi <393669798@qq.com>
2024-04-20 20:00:53 +0800

    Add random seed initialization in main.py

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ from src.data_generator import data_generator
 from src.train import train
 from src.test import test
 
+import torch
+import numpy as np
+import random
+
+def seed_everything(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # 如果使用多个 GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    print(f'Random seed {seed} has been set.')
 
 if __name__ == '__main__':
+    seed = 42  # 可以选择任何喜欢的数字作为种子
+    seed_everything(seed)
+
     # for i in range(1, 6+1):
     #     frame_processing(str_index = str(i))
     # data_generator()
@@ -12,8 +28,7 @@ if __name__ == '__main__':
     # for i in range(1, 6+1):
     #     test(str_index = str(i))
     
-    
     # processed_data_paths = frame_processing()
-    # data_generator()
-    model_save_path = train(num_epochs = 100)
-    test(orientations = 'y')
+    # data_generator(20000,2000,2000)
+    # model_save_path = train(num_epochs = 500, if_use_scheduler = True)
+    test(orientations = 'y', model_load_path = 'archive/seresnet_test_0.0071/seresnet_val_0.0061.pth')
